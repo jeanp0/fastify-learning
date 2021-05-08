b86b99b0a3ace7beb7151de6de5fd4e855233198
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-05-07 21:31:48 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// 'use strict'
+
+const fastify = require("fastify")({
+    logger: true
+})
+
+// module.exports = async function (fastify, opts) {
+fastify.register(require('./db-connector'))
+fastify.register(require('./first-route'))
+// }
+
+fastify.listen(3000, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`)
+})
